c8022893763fa384878a65fea4d88d821b0b7eb7
replicant <vova15122004@mail.ru>
2023-03-14 11:59:34 +0000

    Fix cleaning bug

--- rock-paper.js
+++ rock-paper.js
@@ -91,10 +91,12 @@ function accessImage(){
     let img = document.querySelector(".nested-img");
     return img;
 }
+let playedOnce = 0;
 const choices = document.querySelectorAll(".choice");
 const space = document.querySelector("#image-space");
 choices.forEach(element => {
-    element.addEventListener('click', () => {clearGameMessages(); game(element.textContent)});
+    element.addEventListener('click', () => {if (playedOnce == 1){clearGameMessages();} else {playedOnce = 1;} 
+    game(element.textContent);});
     element.addEventListener('mouseenter', () => space.appendChild(rightImage(element)));
     element.addEventListener('mouseout', () => space.removeChild(accessImage()));
 });
